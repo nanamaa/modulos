ad868a2ce1510107de010359858505153737917d
Mariana <mariana.coatl1826@uppuebla.edu.mx>
2024-05-13 08:55:16 -0600

    Main y módulo creados

---
+++ Main.py
@@ -0,0 +1,5 @@
+from Mimodulo import Sensor
+
+if __name__=="__main__":
+    obj=Sensor()
+    print(obj.pin)
---
+++ Mimodulo.py
@@ -0,0 +1,7 @@
+class Sensor:
+    def __init__(self):
+        self.pin=15
+    def inicializar(self,tipo):
+        self.tipo=tipo
+
+
